f51e2fb39f8e4d7df03fad5d76c512e158cd10a5
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-11-24 02:34:31 +0100

    Added unit tests for BookIntroduction component

--- src/app/components/BookIntroduction/BookIntroduction.spec.js
+++ src/app/components/BookIntroduction/BookIntroduction.spec.js
@@ -1,18 +1,35 @@
 import angular from 'angular';
 import 'angular-mocks';
-import {bookIntroduction} from './BookIntroduction';
+import bookIntroductionModule from './BookIntroduction';
 
 describe('bookIntroduction component', () => {
+  let element;
+  let $scope;
+
   beforeEach(() => {
-    angular
-      .module('bookIntroduction', ['app/components/BookIntroduction/BookIntroduction.html'])
-      .component('bookIntroduction', bookIntroduction);
-    angular.mock.module('bookIntroduction');
+    angular.mock.module(bookIntroductionModule.name);
+
+    angular.mock.inject(($rootScope, $compile) => {
+      $scope = $rootScope.$new();
+      $scope.introduction = [
+        {
+          content: '123'
+        },
+        {
+          content: '234'
+        },
+        {
+          content: '345'
+        }
+      ];
+
+      element = $compile('<book-introduction data-introduction="introduction"></book-introduction>')($scope);
+      $rootScope.$digest();
+    });
   });
 
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
-    const element = $compile('<bookIntroduction></bookIntroduction>')($rootScope);
-    $rootScope.$digest();
-    expect(element).not.toBeNull();
-  }));
+  it('should render 3 paragraphs', () => {
+    const listElements = angular.element(element[0].querySelectorAll('p.md-body-1'));
+    expect(listElements.length).toBe(3);
+  });
 });
