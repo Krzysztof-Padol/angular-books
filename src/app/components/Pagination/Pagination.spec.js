fbeb17d2a283d554e4e12a5310dc28f6a9bb92ab
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-11-24 02:51:54 +0100

    Updated unit tests for Pagination component

--- src/app/components/Pagination/Pagination.spec.js
+++ src/app/components/Pagination/Pagination.spec.js
@@ -1,10 +1,19 @@
 import angular from 'angular';
 import 'angular-mocks';
 import paginationModule from './Pagination';
+import {QUERY_STRING_NAME} from './../../constants/Pagination.js';
 
 describe('pagination component', () => {
+  let $locationMock;
+  const initQueryString = {};
+
   beforeEach(() => {
+    $locationMock = jasmine.createSpyObj('$location', ['search']);
+    $locationMock.search.and.returnValue(initQueryString);
     angular.mock.module(paginationModule.name);
+    angular.mock.module($provide => {
+      $provide.value('$location', $locationMock);
+    });
   });
 
   it('should be rendered', angular.mock.inject(($rootScope, $compile) => {
@@ -51,5 +60,26 @@ describe('pagination component', () => {
     const component = $componentController('pagination', {}, bindings);
     component.prevPage();
     expect(component.currentPage).toBe(0);
+  }));
+
+  it('should update query string if page is changed', angular.mock.inject($componentController => {
+    const bindings = {
+      currentPage: 0,
+      pagesNumber: 5
+    };
+    const component = $componentController('pagination', {}, bindings);
+    component.nextPage();
+    expect($locationMock.search).toHaveBeenCalledWith(QUERY_STRING_NAME, 1);
+  }));
+
+  it('should remove query string if page is on index 0', angular.mock.inject($componentController => {
+    const bindings = {
+      currentPage: 0,
+      pagesNumber: 5
+    };
+    const component = $componentController('pagination', {}, bindings);
+    component.nextPage();
+    component.prevPage();
+    expect($locationMock.search).toHaveBeenCalledWith(QUERY_STRING_NAME, null);
   }));
 });
