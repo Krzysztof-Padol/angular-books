bcd9cd7435e9739adef04e6684b5076092f51d53
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-11-24 03:02:42 +0100

    Added unit tests for SearchHeader component

--- src/app/components/SearchHeader/SearchHeader.spec.js
+++ src/app/components/SearchHeader/SearchHeader.spec.js
@@ -1,18 +1,77 @@
 import angular from 'angular';
 import 'angular-mocks';
-import {searchHeader} from './SearchHeader';
+import searchHeaderModule from './SearchHeader';
 
 describe('searchHeader component', () => {
+  let $locationMock;
+  const initQueryString = {};
+
   beforeEach(() => {
-    angular
-      .module('searchHeader', ['app/components/SearchHeader/SearchHeader.html'])
-      .component('searchHeader', searchHeader);
-    angular.mock.module('searchHeader');
+    $locationMock = jasmine.createSpyObj('$location', ['search']);
+    $locationMock.search.and.returnValue(initQueryString);
+    angular.mock.module(searchHeaderModule.name);
+    angular.mock.module($provide => {
+      $provide.value('$location', $locationMock);
+    });
   });
 
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
-    const element = $compile('<searchHeader></searchHeader>')($rootScope);
-    $rootScope.$digest();
-    expect(element).not.toBeNull();
+  it('should call onFilterChange function', angular.mock.inject($componentController => {
+    const bindings = {
+      onFilterChange: () => {}
+    };
+    const component = $componentController('searchHeader', {}, bindings);
+    spyOn(component, 'onFilterChange').and.callThrough();
+    component.handleDataChange();
+    expect(component.onFilterChange).toHaveBeenCalledWith({
+      filters: {
+        genre: '',
+        category: '',
+        search: ''
+      }
+    });
+  }));
+
+  it('should call onFilterChange function', angular.mock.inject($componentController => {
+    const newValue = "newValueStr";
+    const bindings = {
+      onFilterChange: () => {}
+    };
+    const component = $componentController('searchHeader', {}, bindings);
+    spyOn(component, 'onFilterChange').and.callThrough();
+    component.filters.search = newValue;
+    component.handleDataChange();
+    expect(component.onFilterChange).toHaveBeenCalledWith({
+      filters: {
+        genre: '',
+        category: '',
+        search: newValue
+      }
+    });
+  }));
+
+  it('should update query string', angular.mock.inject($componentController => {
+    const newValue = "newValueStr";
+    const bindings = {
+      onFilterChange: () => {}
+    };
+    const component = $componentController('searchHeader', {}, bindings);
+    spyOn(component, 'onFilterChange').and.callThrough();
+    component.filters.search = newValue;
+    component.handleDataChange();
+    expect($locationMock.search).toHaveBeenCalledWith('search', newValue);
+  }));
+
+  it('should remove query string', angular.mock.inject($componentController => {
+    const newValue = "newValueStr";
+    const bindings = {
+      onFilterChange: () => {}
+    };
+    const component = $componentController('searchHeader', {}, bindings);
+    spyOn(component, 'onFilterChange').and.callThrough();
+    component.filters.search = newValue;
+    component.handleDataChange();
+    component.filters.search = '';
+    component.handleDataChange();
+    expect($locationMock.search).toHaveBeenCalledWith('search', null);
   }));
 });
