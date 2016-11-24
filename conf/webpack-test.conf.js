fdf0d1f148d0e577d2010b6c85988757a13ad20f
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-11-24 01:51:30 +0100

    Added unit tests to author avatar

--- conf/webpack-test.conf.js
+++ conf/webpack-test.conf.js
@@ -16,6 +16,26 @@ module.exports = {
         ]
       },
       {
+        test: /\.(css|scss)$/,
+        include: [/node_modules/],
+        loaders: [
+          'style',
+          'css',
+          'sass',
+          'postcss'
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: [/node_modules/],
+        loaders: [
+          'style',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'sass',
+          'postcss'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loaders: [
--- src/app/components/AuthorAvatar/AuthorAvatar.spec.js
+++ src/app/components/AuthorAvatar/AuthorAvatar.spec.js
@@ -1,18 +1,45 @@
 import angular from 'angular';
 import 'angular-mocks';
-import {authorAvatar} from './AuthorAvatar';
+import authorAvatarModule from './AuthorAvatar';
 
 describe('authorAvatar component', () => {
   beforeEach(() => {
-    angular
-      .module('authorAvatar', ['app/components/AuthorAvatar/AuthorAvatar.html'])
-      .component('authorAvatar', authorAvatar);
-    angular.mock.module('authorAvatar');
+    angular.mock.module(authorAvatarModule.name);
   });
 
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
-    const element = $compile('<authorAvatar></authorAvatar>')($rootScope);
+  it('should show name of author', angular.mock.inject(($rootScope, $compile) => {
+    const $scope = $rootScope.$new();
+    $scope.author = {
+      name: 'Example name'
+    };
+    const element = $compile('<author-avatar author="author"></author-avatar>')($scope);
     $rootScope.$digest();
-    expect(element).not.toBeNull();
+
+    const name = element.find('h4');
+    expect(name.html().trim()).toEqual(`by ${$scope.author.name}`);
+  }));
+
+  it('should render image', angular.mock.inject(($rootScope, $compile) => {
+    const $scope = $rootScope.$new();
+    $scope.author = {
+      avatar: 'http://example.com/img.jpg'
+    };
+    const element = $compile('<author-avatar author="author"></author-avatar>')($scope);
+    $rootScope.$digest();
+
+    const img = element.find('img');
+    expect(img.attr('src')).toEqual(`${$scope.author.avatar}`);
+  }));
+
+  it('should hr', angular.mock.inject(($rootScope, $compile) => {
+    const $scope = $rootScope.$new();
+    $scope.author = {
+      avatar: 'http://example.com/img.jpg'
+    };
+    const element = $compile('<author-avatar author="author"></author-avatar>')($scope);
+    $rootScope.$digest();
+
+    const hr = element.find('hr');
+    expect(hr).toBeDefined();
   }));
 });
