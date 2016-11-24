7c2e98a3f4e304a8449c6a9e9e471cd5c98414aa
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-11-24 02:20:52 +0100

    Added unit tests for BookCard component

--- src/app/components/AuthorAvatar/AuthorAvatar.spec.js
+++ src/app/components/AuthorAvatar/AuthorAvatar.spec.js
@@ -3,43 +3,36 @@ import 'angular-mocks';
 import authorAvatarModule from './AuthorAvatar';
 
 describe('authorAvatar component', () => {
+  let element;
+  let $scope;
+
   beforeEach(() => {
     angular.mock.module(authorAvatarModule.name);
+
+    angular.mock.inject(($rootScope, $compile) => {
+      $scope = $rootScope.$new();
+      $scope.author = {
+        name: 'Example name',
+        avatar: 'http://example.com/img.jpg'
+      };
+
+      element = $compile('<author-avatar author="author"></author-avatar>')($scope);
+      $rootScope.$digest();
+    });
   });
 
-  it('should show name of author', angular.mock.inject(($rootScope, $compile) => {
-    const $scope = $rootScope.$new();
-    $scope.author = {
-      name: 'Example name'
-    };
-    const element = $compile('<author-avatar author="author"></author-avatar>')($scope);
-    $rootScope.$digest();
-
+  it('should show name of author', () => {
     const name = element.find('h4');
     expect(name.html().trim()).toEqual(`by ${$scope.author.name}`);
-  }));
+  });
 
-  it('should render image', angular.mock.inject(($rootScope, $compile) => {
-    const $scope = $rootScope.$new();
-    $scope.author = {
-      avatar: 'http://example.com/img.jpg'
-    };
-    const element = $compile('<author-avatar author="author"></author-avatar>')($scope);
-    $rootScope.$digest();
-
+  it('should render image', () => {
     const img = element.find('img');
     expect(img.attr('src')).toEqual(`${$scope.author.avatar}`);
-  }));
+  });
 
-  it('should hr', angular.mock.inject(($rootScope, $compile) => {
-    const $scope = $rootScope.$new();
-    $scope.author = {
-      avatar: 'http://example.com/img.jpg'
-    };
-    const element = $compile('<author-avatar author="author"></author-avatar>')($scope);
-    $rootScope.$digest();
-
+  it('should render hr', () => {
     const hr = element.find('hr');
     expect(hr).toBeDefined();
-  }));
+  });
 });
--- src/app/components/BookCard/BookCard.spec.js
+++ src/app/components/BookCard/BookCard.spec.js
@@ -1,18 +1,53 @@
 import angular from 'angular';
 import 'angular-mocks';
-import {bookCard} from './BookCard';
+import bookCardModule from './BookCard';
 
 describe('bookCard component', () => {
+  let element;
+  let $scope;
+  const someDateMock = "2012-05-26T06:30:00";
+
   beforeEach(() => {
-    angular
-      .module('bookCard', ['app/components/BookCard/BookCard.html'])
-      .component('bookCard', bookCard);
-    angular.mock.module('bookCard');
+    angular.mock.module(bookCardModule.name);
+
+    angular.mock.inject(($rootScope, $compile) => {
+      jasmine.clock().install(); // mocking self.today value in service
+      jasmine.clock().mockDate(new Date(someDateMock));
+
+      $scope = $rootScope.$new();
+      $scope.book = {
+        name: 'Example name of book',
+        cover: '/img.jpg',
+        likes: 12345,
+        published: '2003-09-18T01:59:14.918Z'
+      };
+
+      element = $compile('<book-card data-book="book"></book-card>')($scope);
+      $rootScope.$digest();
+    });
   });
 
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
-    const element = $compile('<bookCard></bookCard>')($rootScope);
-    $rootScope.$digest();
-    expect(element).not.toBeNull();
-  }));
+  it('should render cover image', () => {
+    const img = element.find('img');
+    expect(img.attr('src')).toEqual(`${$scope.book.cover}`);
+  });
+
+  it('should render book name', () => {
+    const span = angular.element(element[0].querySelectorAll('span.md-headline'));
+    expect(span.html().trim()).toEqual(`${$scope.book.name}`);
+  });
+
+  it('should render number of likes', () => {
+    const p = angular.element(element[0].querySelectorAll('p.md-body-1'));
+    expect(p.html().trim()).toEqual(`${$scope.book.likes}`);
+  });
+
+  it('should render render relative time to publish date', () => {
+    const span = angular.element(element[0].querySelectorAll('span.md-body-1'));
+    expect(span.html().trim()).toEqual('9 years ago');
+  });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
 });
