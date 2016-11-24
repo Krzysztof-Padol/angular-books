fb975c457e7184cbe00876fe6b9981967db50bda
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-11-24 02:30:20 +0100

    Added unit tests for BookDetailsHeader component

--- src/app/components/BookDetailsHeader/BookDetailsHeader.spec.js
+++ src/app/components/BookDetailsHeader/BookDetailsHeader.spec.js
@@ -1,18 +1,42 @@
 import angular from 'angular';
 import 'angular-mocks';
-import {bookDetailsHeader} from './BookDetailsHeader';
+import bookDetailsModule from './BookDetailsHeader';
 
 describe('bookDetailsHeader component', () => {
+  let element;
+  let $scope;
+
   beforeEach(() => {
-    angular
-      .module('bookDetailsHeader', ['app/components/BookDetailsHeader/BookDetailsHeader.html'])
-      .component('bookDetailsHeader', bookDetailsHeader);
-    angular.mock.module('bookDetailsHeader');
+    angular.mock.module(bookDetailsModule.name);
+
+    angular.mock.inject(($rootScope, $compile) => {
+      $scope = $rootScope.$new();
+      $scope.book = {
+        name: 'Example name',
+        avatar: 'http://example.com/img.jpg',
+        description: 'Example description',
+        author: {
+          name: 'Some Author'
+        }
+      };
+
+      element = $compile('<book-details-header data-book="book"></book-details-header>')($scope);
+      $rootScope.$digest();
+    });
   });
 
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
-    const element = $compile('<bookDetailsHeader></bookDetailsHeader>')($rootScope);
-    $rootScope.$digest();
-    expect(element).not.toBeNull();
-  }));
+  it('should render book name', () => {
+    const h1 = element.find('h1');
+    expect(h1.html().trim()).toEqual(`${$scope.book.name}`);
+  });
+
+  it('should render author name', () => {
+    const h4 = element.find('h4');
+    expect(h4.html().trim()).toEqual(`by ${$scope.book.author.name}`);
+  });
+
+  it('should render book name', () => {
+    const p = angular.element(element[0].querySelectorAll('p.md-subhead'));
+    expect(p.html().trim()).toEqual(`${$scope.book.description}`);
+  });
 });
